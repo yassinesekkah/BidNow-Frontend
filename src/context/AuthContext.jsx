d836e5656a3c2b07082221d9ca1c87b780a0150d
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-04-09 14:53:35 +0100

    feat(auth): add AuthContext for global authentication state

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useEffect, useState } from "react";
+import { getUser } from "../services/authService";
+
+//hna kandiro l creation dyal context
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const fetchUser = async () => {
+        try{
+            const res = await getUser();
+            setUser(res.data);
+        }catch(err){
+            setUser(null);
+        }finally{
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, loading}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
